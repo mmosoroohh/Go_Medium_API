14c9b4a7f86d2ab4343314722fe76b524f0a9717
mmosoroohh <arnoldmaengwe@gmail.com>
2019-12-16 18:03:21 +0300

    (Medium API): A simple Medium API using Golang  - Create models  - Create controllers  - Create middlewares  - Create authentication  - Write tests

---
+++ tests/controller/controller_test.go
@@ -0,0 +1,157 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+	"github.com/mmosoroohh/Go_Medium_API/api/controllers"
+	"github.com/mmosoroohh/Go_Medium_API/api/models"
+	"log"
+	"os"
+	"testing"
+)
+
+var server = controllers.Server{}
+var user = models.User{}
+var post = models.Post{}
+
+func TestMain(m *testing.M) {
+	var err error
+	err = godotenv.Load(os.ExpandEnv("../../.env"))
+	if err != nil {
+		log.Fatalf("Error occurred while getting env %v\n", err)
+	}
+	Database()
+
+	os.Exit(m.Run())
+}
+
+func Database() {
+
+	var err error
+
+	TestDbDriver := os.Getenv("TestDbDriver")
+
+	if TestDbDriver == "mysql" {
+		DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("TestDbUser"), os.Getenv("TestDbPassword"), os.Getenv("TestDbHost"), os.Getenv("TestDbPort"), os.Getenv("TestDbName"))
+		server.DB, err = gorm.Open(TestDbDriver, DBURL)
+		if err != nil {
+			fmt.Printf("Error occurred connecting to %s database\n", TestDbDriver)
+			log.Fatal("Error occurred: ", err)
+		} else {
+			fmt.Printf("We are connected to the %s database\n", TestDbDriver)
+		}
+	}
+
+	if TestDbDriver == "postgres" {
+		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", os.Getenv("TestDbHost"), os.Getenv("TestDbPort"), os.Getenv("TestDbUser"), os.Getenv("TestDbName"), os.Getenv("TestDbPassword"))
+		server.DB, err = gorm.Open(TestDbDriver, DBURL)
+		if err != nil {
+			fmt.Printf("Error occurred connecting to %s database\n", TestDbDriver)
+			log.Fatal("Error occurred: ", err)
+		} else {
+			fmt.Printf("We are connected to the %s database\n", TestDbDriver)
+		}
+	}
+}
+
+func refreshUserTable() error {
+	err := server.DB.DropTableIfExists(&models.User{}).Error
+	if err != nil {
+		return err
+	}
+	err = server.DB.AutoMigrate(&models.User{}).Error
+	if err != nil {
+		return err
+	}
+	log.Printf("Successfully refreshed table")
+	return nil
+}
+
+func seedUser() (models.User, error) {
+
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user := models.User{
+		Username: "Bond",
+		Email:    "jamebond@gmail.com",
+		Password: "Password",
+	}
+
+	err = server.DB.Model(&models.User{}).Create(&user).Error
+	if err != nil {
+		return models.User{}, err
+	}
+	return user, nil
+}
+
+func seedUsers() ([]models.User, error) {
+
+	var err error
+	if err != nil {
+		return nil, err
+	}
+	users := []models.User{
+		models.User{
+			Username: "mmosorooh",
+			Email:    "mmosoroohh@gmail.com",
+			Password: "password",
+		},
+		models.User{
+			Username: "osoro",
+			Email:    "osoro@gmail.com",
+			Password: "password",
+		},
+	}
+	for i, _ := range users {
+		err := server.DB.Model(&models.User{}).Create(&users[i]).Error
+		if err != nil {
+			return []models.User{}, err
+		}
+	}
+	return users, nil
+}
+
+func refreshUserAndPostTable() error {
+
+	err := server.DB.DropTableIfExists(&models.User{}, &models.Post{}).Error
+	if err != nil {
+		return err
+	}
+	err = server.DB.AutoMigrate(&models.User{}, &models.Post{}).Error
+	if err != nil {
+		return err
+	}
+	log.Printf("Successfully refreshed tables")
+	return nil
+}
+
+func seedOneUserAndOnePost() (models.Post, error) {
+
+	err := refreshUserAndPostTable()
+	if err != nil {
+		return models.Post{}, err
+	}
+	user := models.User{
+		Username: "bryan",
+		Email:    "bryan@gmail.com",
+		Password: "password",
+	}
+	err = server.DB.Model(&models.User{}).Create(&user).Error
+	if err != nil {
+		return models.Post{}, err
+	}
+	post := models.Post{
+		Title:    "This is the title",
+		Content:  "This is the content",
+		AuthorID: user.ID,
+	}
+	err = server.DB.Model(&models.Post{}).Create(&post).Error
+	if err != nil {
+		return models.Post{}, err
+	}
+	return post, nil
+}
