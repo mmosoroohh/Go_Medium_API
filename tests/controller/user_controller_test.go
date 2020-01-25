870123a41b6d410b3d12831a1a18c7c787236025
mmosoroohh <arnoldmaengwe@gmail.com>
2020-01-25 18:50:51 +0300

    (Medium API): A simple Medium API using Golang  - Create models  - Create controllers  - Create middlewares  - Create authentication  - Write tests.

--- tests/controller/user_controller_test.go
+++ tests/controller/user_controller_test.go
@@ -4,11 +4,13 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/mmosoroohh/Go_Medium_API/api/models"
 	"gopkg.in/go-playground/assert.v1"
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 )
 
@@ -119,4 +121,213 @@ func TestGetUsers(t *testing.T) {
 	}
 	assert.Equal(t, rec.Code, http.StatusOK)
 	assert.Equal(t, len(users), 2)
+}
+
+func TestGetUser(t *testing.T) {
+
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	person, err := seedUser()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sampleUser := []struct {
+		id           string
+		statusCode   int
+		username     string
+		email        string
+		errorMessage string
+	}{
+		{
+			id:         strconv.Itoa(int(person.ID)),
+			statusCode: 200,
+			username:   person.Username,
+			email:      person.Email,
+		},
+		{
+			id:         "unknown",
+			statusCode: 400,
+		},
+	}
+
+	for _, v := range sampleUser {
+
+		req, err := http.NewRequest("GET", "/users", nil)
+		if err != nil {
+			t.Errorf("Error occurred: %v\n", err)
+		}
+		req = mux.SetURLVars(req, map[string]string{"id": v.id})
+		rec := httptest.NewRecorder()
+		handler := http.HandlerFunc(server.GetUser)
+		handler.ServeHTTP(rec, req)
+
+		responseMap := make(map[string]interface{})
+		err = json.Unmarshal([]byte(rec.Body.String()), &responseMap)
+		if err != nil {
+			log.Fatal("Error occurred converting to json: %v", err)
+		}
+
+		assert.Equal(t, rec.Code, v.statusCode)
+
+		if v.statusCode == 200 {
+			assert.Equal(t, person.Username, responseMap["username"])
+			assert.Equal(t, person.Email, responseMap["email"])
+		}
+	}
+}
+
+func TestUpdateUser(t *testing.T) {
+
+	var AuthEmail, AuthPassword string
+	var AuthID uint32
+
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	users, err := seedUsers() //we need atleast two users to properly check the update
+	if err != nil {
+		log.Fatal("Error occurred seeding the users: %v\n", err)
+	}
+	// Get only the first user
+	for _, user := range users {
+		if user.ID == 2 {
+			continue
+		}
+		AuthID = user.ID
+		AuthEmail = user.Email
+		AuthPassword = "password"
+		// Note the password is the database is already hashed, we want unhashed password
+	}
+	// Login the user and get the authentication token
+	token, err := server.SignIn(AuthEmail, AuthPassword)
+	if err != nil {
+		log.Fatal("Error occurred cannot login: %v\n", err)
+	}
+	tokenString := fmt.Sprintf("Bearer %v", token)
+
+	samples := []struct {
+		id             string
+		updateJSON     string
+		statusCode     int
+		updateUsername string
+		updateEmail    string
+		tokenGiven     string
+		errorMessage   string
+	}{
+		{
+			// Convert int32 to int first before converting to a string
+			id:             strconv.Itoa(int(AuthID)),
+			updateJSON:     `{"username": "mmosoroohh", "email": "mmosoroohh@gmail.com", "password": "password"}`,
+			statusCode:     200,
+			updateUsername: "mmosoroohh",
+			updateEmail:    "mmosoroohh@gmail.com",
+			tokenGiven:     tokenString,
+			errorMessage:   "",
+		},
+		{
+			// When password field is empty
+			id:           strconv.Itoa(int(AuthID)),
+			updateJSON:   `{"nickname": "joe", "email": "joe@gmail.com", "password": ""}`,
+			statusCode:   422,
+			tokenGiven:   tokenString,
+			errorMessage: "Password Required",
+		},
+		{
+			// When No token is provided
+			id:           strconv.Itoa(int(AuthID)),
+			updateJSON:   `{"username": "mary", "email": "mary@gmail.com", "password": "password"}`,
+			statusCode:   401,
+			tokenGiven:   "",
+			errorMessage: "Unauthorized",
+		},
+		{
+			// When an incorrect token is passed
+			id:           strconv.Itoa(int(AuthID)),
+			updateJSON:   `{"username": "brian", "email": "brian@gmail.com", "password": "password"}`,
+			statusCode:   401,
+			tokenGiven:   "This is an incorrect token",
+			errorMessage: "Unauthorized",
+		},
+		{
+			// Remember "joedoe@gmail.com" belongs to user two
+			id:           strconv.Itoa(int(AuthID)),
+			updateJSON:   `{"username": "joedoe", "email": "joedoe@gmail.com", "password": "password"}`,
+			statusCode:   500,
+			tokenGiven:   tokenString,
+			errorMessage: "Email Already Exist",
+		},
+		{
+			// Remember "John Doe" belongs to user two
+			id:           strconv.Itoa(int(AuthID)),
+			updateJSON:   `{"username": "joedoe", "email": "mmosoroohh@gmail.com", "password": "password"}`,
+			statusCode:   500,
+			tokenGiven:   tokenString,
+			errorMessage: "Username Already Exists",
+		},
+		{
+			id:           strconv.Itoa(int(AuthID)),
+			updateJSON:   `{"username": "mmosoroohh", "email": "mmosoroohh@gmail.com", "password": "password"}`,
+			statusCode:   422,
+			tokenGiven:   tokenString,
+			errorMessage: "Invalid Email",
+		},
+		{
+			id:           strconv.Itoa(int(AuthID)),
+			updateJSON:   `{"username": "", "email": "mmosoroohh@gmail.com", "password": "password"}`,
+			statusCode:   422,
+			tokenGiven:   tokenString,
+			errorMessage: "Username Required",
+		},
+		{
+			id:           strconv.Itoa(int(AuthID)),
+			updateJSON:   `{"username": "mmosoroohh", "email": "", "password": "password"}`,
+			statusCode:   422,
+			tokenGiven:   tokenString,
+			errorMessage: "Email Required",
+		},
+		{
+			id:         "unknown",
+			tokenGiven: tokenString,
+			statusCode: 400,
+		},
+	}
+
+	for _, v := range samples {
+
+		req, err := http.NewRequest("POST", "/users", bytes.NewBufferString(v.updateJSON))
+		if err != nil {
+			t.Errorf("Error Occurred: %v\n", err)
+		}
+
+		req = mux.SetURLVars(req, map[string]string{"id": v.id})
+
+		rec := httptest.NewRecorder()
+		handler := http.HandlerFunc(server.UpdateUser)
+
+		req.Header.Set("Authorization", v.tokenGiven)
+
+		handler.ServeHTTP(rec, req)
+
+		responseMap := make(map[string]interface{})
+		err = json.Unmarshal([]byte(rec.Body.String()), &responseMap)
+		if err != nil {
+			t.Errorf("Error Occurred converting to json: %v", err)
+		}
+
+		assert.Equal(t, rec.Code, v.statusCode)
+		if v.statusCode == 200 {
+			assert.Equal(t, responseMap["username"], v.updateUsername)
+			assert.Equal(t, responseMap["email"], v.updateEmail)
+		}
+
+		if v.statusCode == 401 || v.statusCode == 422 || v.statusCode == 500 && v.errorMessage != "" {
+			assert.Equal(t, responseMap["error"], v.errorMessage)
+		}
+	}
 }
